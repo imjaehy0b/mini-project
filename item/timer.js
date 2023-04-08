ae2a11dd890f3269dcb3631278b2e6258b8ea900
durdleRain <durdleRain@gmail.com>
2023-04-08 19:40:32 +0900

    오류 수정

--- item/timer.js
+++ item/timer.js
@@ -1,17 +1,25 @@
 export default class Timer {
+  #time
   constructor() {
-    this.time = 3600;
+    this.#time = 60;
   }
 
-  decreaseTimer() {
-    if (this.time > 0) {
-      this.time--;
-      document.querySelector('#timer').innerHTML = Math.floor(this.time / 60);
+  decreaseTime() {
+    if (this.#time > 0) {
+      this.#time--;
+      document.querySelector('#timer').innerHTML = Math.floor(this.#time / 60);
     }
+  }
+
+  checkTime() {
+    if (this.#time == 0) {
+      return true;
+    }
+  }
+  
     // if (this.timer === 0) {
     //     if () {
     //         gameover 띄우기
     //     }
     // }
   }
-}
