6faad670f440fc9c528f5b64986418636d677a8b
imjae <imjae950@gmail.com>
2023-04-10 16:47:51 +0900

    event listener 수정

--- item/background.js
+++ item/background.js
@@ -4,30 +4,32 @@ export default class BackGround {
   #y;
   #speedModifier
   #image
+  
+
   constructor(image, movSpeed, y_Position) {
     this.#speedModifier = movSpeed;
     this.#image = image;
     this.#x = 0;
-    this.#x2 = 768;
+    this.#x2 = 1024;
     this.#y = y_Position;
   }
 
   update() {
     let speed = this.#speedModifier;
-    if (this.#x < -768) {
-      this.#x = 768 - speed + this.#x2;
+    if (this.#x < -1024) {
+      this.#x = 1024 - speed + this.#x2;
     } else {
       this.#x -= speed;
     }
 
-    if (this.#x2 < -768) {
-      this.#x2 = 768 - speed + this.#x;
+    if (this.#x2 < -1024) {
+      this.#x2 = 1024 - speed + this.#x;
     } else {
       this.#x2 -= speed;
     }
   }
   draw(ctx) {
-    ctx.drawImage(this.#image, this.#x, this.#y,768,512);
-    ctx.drawImage(this.#image, this.#x2, this.#y,768,512);
+    ctx.drawImage(this.#image, this.#x, this.#y,1024,640);
+    ctx.drawImage(this.#image, this.#x2, this.#y,1024,640);
   }
 }
